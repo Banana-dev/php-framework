73aa15a466311b41de944b958aaf24e70e2a8294
VANNEAU Jeremy <Jeremy.VANNEAU@groupe.berger-levrault.net>
2018-01-22 16:23:39 +0100

    premier essaie vue

--- src/controller/PagesController.php
+++ src/controller/PagesController.php
@@ -3,11 +3,33 @@
 namespace App\Controller;
 
 use Banana\Controller\BaseController;
-
+use template\template;
+use Article;
 class PagesController extends BaseController
 {
     public function index()
     {
-        echo "Ok";
+        
+		 
+		// $object = new stdClass();
+		// $object->attr = 'Je suis un attribut';
+		 
+		$article = new Article();
+		 
+		$arr = [
+			'pays' => 'france',
+			'langue' => 'français',
+			'departements' => [
+				'59' => 'Nord',
+				'60' => 'Oise'
+			]
+		];
+		 
+		$tpl = new Tpl('index.tpl');
+		$tpl->set('sitename', 'Mon super site');
+		$tpl->set('obj', $object);
+		$tpl->set('tableau', $arr);
+		$tpl->set('ARTICLE', $article);
+		echo $tpl->output();
     }
 }
